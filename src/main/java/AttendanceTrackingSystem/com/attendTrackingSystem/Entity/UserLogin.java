a1cc868c34a58403fa7d14c82baf32da9ed4d36d
Dell <Dell@DESKTOP-J12IGTA>
2025-06-29 18:39:09 +0530

    done

--- src/main/java/AttendanceTrackingSystem/com/attendTrackingSystem/Entity/UserLogin.java
+++ src/main/java/AttendanceTrackingSystem/com/attendTrackingSystem/Entity/UserLogin.java
@@ -1,5 +1,14 @@
 package AttendanceTrackingSystem.com.attendTrackingSystem.Entity;
 
 public class UserLogin {
+	private String username;
+	private String password;
+	
+	public UserLogin() {}
+	
+	public UserLogin(String username,String password) {
+		this.username=username;
+		this.password=password;
+	}
 
 }
